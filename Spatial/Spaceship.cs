612fada9a4493f4cf12b6b284a8ae3a28b6473b8
Florian LE GOFF <contact@florianlegoff.com>
2017-01-31 14:36:50 +0100

    Custom classes creation

---
+++ Spatial/Spaceship.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Userflux
+{
+    class Spaceship
+    {
+        private int id;
+        private string couleur;
+        private int vitesse;
+        private string agence;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Couleur
+        {
+            get
+            {
+                return couleur;
+            }
+
+            set
+            {
+                couleur = value;
+            }
+        }
+
+        public int Vitesse
+        {
+            get
+            {
+                return vitesse;
+            }
+
+            set
+            {
+                vitesse = value;
+            }
+        }
+
+        public string Agence
+        {
+            get
+            {
+                return agence;
+            }
+
+            set
+            {
+                agence = value;
+            }
+        }
+    }
+}
